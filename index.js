1103bef2efcf51faf1f30d10d17d8d30f390ec96
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-10-29 08:54:37 +0300

    add routes middleware to app entry point

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const connection = require('./db');
+require('./db');
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 const port = process.env.PORT || 5000;
 
-//app.use(routes);
-app.get('/',(req,res)=>res.send({message:'hello express is working'}));
+app.use(userRoutes);
+app.use(postRoutes);
+app.get('/', (req, res) => res.send({ message: 'hello express is working' }));
 app.listen(port, () => console.log(`The app is listening on port ${port}`));
