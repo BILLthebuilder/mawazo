061738219769e03f11b87e15b26ec0b3e3e9e03a
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-10-29 20:47:44 +0300

    add the comment route middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db');
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
+const commentRoutes = require('./routes/comment');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -11,5 +12,6 @@ const port = process.env.PORT || 5000;
 
 app.use(userRoutes);
 app.use(postRoutes);
+app.use(commentRoutes);
 app.get('/', (req, res) => res.send({ message: 'hello express is working' }));
 app.listen(port, () => console.log(`The app is listening on port ${port}`));
