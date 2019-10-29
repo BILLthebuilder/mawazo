09a01002fab63cd038373322298d8559efb758f2
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-10-29 10:56:32 +0300

    add edit user endpoint

--- routes/user.js
+++ routes/user.js
@@ -27,4 +27,33 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+// Enable a user to view their own user profile details
+router.get('/users/me', authenticate, async (req, res) => {
+    res.send(req.user);
+});
+
+// Edit user profile details
+router.patch('/users/me', authenticate, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+    const { _id } = req.user;
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid request' });
+    }
+
+    if (!ObjectID.isValid(_id)) {
+        return res.status(404).send();
+    }
+
+    try {
+        updates.forEach(update => (req.user[update] = req.body[update]));
+        await req.user.save();
+        res.send(req.user);
+    } catch (error) {
+        res.status(400).send();
+    }
+});
+
 module.exports = router;
