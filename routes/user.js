39230da568c3a58e3a71b976d2a8605ccf227957
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-10-29 09:59:55 +0300

    create the login endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const { ObjectID } = require('mongodb');
 const { Router } = require('express');
 const User = require('../models/User');
+const authenticate = require('../auth/auth');
 
 const router = Router();
 
+// Create a new user
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
@@ -11,6 +13,17 @@ router.post('/users', async (req, res) => {
         res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
+    }
+});
+
+// Login a registered user
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.checkValidCredentials(req.body.email, req.body.password);
+        const token = await user.newAuthToken();
+        res.send({ user, token });
+    } catch (error) {
+        res.status(400).send();
     }
 });
 
