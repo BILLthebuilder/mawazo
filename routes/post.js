df35ac4c53fa409490a725a58d44e4a92af24e0c
BILLthebuilder <billkariri@gmail.com>
2019-10-29 12:54:06 +0300

    create posts endpoint

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,19 @@ const { ObjectID } = require('mongodb');
 const { Router } = require('express');
 const Post = require('../models/Post');
 
-const routes = Router();
+const router = Router();
+
+router.post('/posts', authenticate, async (req, res) => {
+    const post = new Post({
+        ...req.body,
+        author: req.user._id
+    });
+    try {
+        await post.save();
+        res.status(201).send(post);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
 
 module.exports = routes;
