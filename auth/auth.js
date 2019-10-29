49c8f15ae9a42d15260866a589837c6fafe16a3c
BILLthebuilder <billkariri@gmail.com>
2019-10-29 10:00:17 +0300

    add authorization middleware

---
+++ auth/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+require('dotenv').config();
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req
+            .header('Authorization')
+            .replace('Bearer', '')
+            .trim();
+
+        const decoded = jwt.verify(token, process.env.SECRET);
+
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {
+            throw new Error('User does not exist');
+        }
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).send({ error: 'Please authenticate!' });
+    }
+};
+
+module.exports = auth;
