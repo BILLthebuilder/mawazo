1177ad61bf77009d0be48e435f4e1a8230156140
BILLthebuilder <billkariri@gmail.com>
2019-10-29 09:05:31 +0300

    add the users route

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,16 @@ const { ObjectID } = require('mongodb');
 const { Router } = require('express');
 const User = require('../models/User');
 
-const routes = Router();
+const router = Router();
 
-module.exports = routes;
+router.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+        const token = await user.newAuthToken();
+        res.status(201).send({ user, token });
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
+module.exports = router;
