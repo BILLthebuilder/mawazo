e3acc75b6d8e1c467366cca3b1b72728acc724e4
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2019-11-01 13:00:28 +0300

    create validation schemas for our api

---
+++ validations/validations.js
@@ -0,0 +1,43 @@
+const Joi = require('@hapi/joi');
+
+const userSchema = Joi.object({
+    name: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(50)
+        .required(),
+    age: Joi.number()
+        .min(13)
+        .max(101)
+        .required(),
+    email: Joi.string()
+        .email({ minDomainSegments: 2 })
+        .required(),
+    password: Joi.string()
+        .regex(/^[a-zA-Z0-9]{3,32}$/)
+        .required()
+});
+
+const postSchema = Joi.object({
+    title: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(50)
+        .required(),
+    description: Joi.string
+        .alphanum()
+        .min(3)
+        .required()
+});
+
+const commentSchema = Joi.object({
+    comment: Joi.string()
+        .alphanum()
+        .required()
+});
+
+module.exports = {
+    userSchema,
+    postSchema,
+    commentSchema
+};
